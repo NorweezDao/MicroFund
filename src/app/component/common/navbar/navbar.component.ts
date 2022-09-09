31743fbcd207dfbab9bd320fe0461c5604d897fd
AntonioCardenas <antonioalx66@gmail.com>
2022-09-08 21:07:30 -0600

    Wallet and Create

--- src/app/component/common/navbar/navbar.component.ts
+++ src/app/component/common/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {LoginComponent} from "../../pages/login/login.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoginComponent],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
